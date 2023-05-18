9f257a11bc9712ee59fc5713706672ab5ab592b8
Tator <617661369@qq.com>
2023-05-19 00:42:48 +0800

    添加短信验证

--- src/main/java/com/seproj/seu_booking_back/controller/AccountController.java
+++ src/main/java/com/seproj/seu_booking_back/controller/AccountController.java
@@ -1,7 +1,10 @@
 package com.seproj.seu_booking_back.controller;//package com.eaproj.seu_booking_project.controller;
 
+import com.seproj.seu_booking_back.service.SendSms;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.SecureRandom;
 import java.util.Map;
 
 @RestController
@@ -10,13 +13,14 @@ import java.util.Map;
 public class AccountController {
 
     @PostMapping("/account")
-    public boolean Register(@RequestBody Map data) {
-        String userName = data.get("user_address").toString();
-        System.out.print(userName);
-        if(userName.equals("1")) {
-            return true;
-        } else {
-            return false;
-        }
+    public String Register(@RequestBody Map data) {
+        SecureRandom randomGen = new SecureRandom();
+        String code = String.format("%04d", randomGen.nextInt(1000));
+        String loginVal = data.get("loginVal").toString();
+        String loginType = data.get("loginType").toString();
+        System.out.print(loginVal + ", " + loginType + '\n');
+
+        SendSms messageSender = new SendSms(loginVal, code);
+        return loginVal + ", " + loginType;
     }
 }
