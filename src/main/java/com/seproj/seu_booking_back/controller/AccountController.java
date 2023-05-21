d82154f63219044e24d53071551c0a865712d8a1
Tator <617661369@qq.com>
2023-05-21 14:33:47 +0800

    添加短信验证

--- src/main/java/com/seproj/seu_booking_back/controller/AccountController.java
+++ src/main/java/com/seproj/seu_booking_back/controller/AccountController.java
@@ -1,11 +1,34 @@
-package com.seproj.seu_booking_back.controller;//package com.eaproj.seu_booking_project.controller;
+package com.seproj.seu_booking_back.controller;
 
-import com.seproj.seu_booking_back.service.SendSms;
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.SecureRandom;
 import java.util.Map;
+
+import com.aliyun.auth.credentials.Credential;
+import com.aliyun.auth.credentials.provider.StaticCredentialProvider;
+import com.aliyun.core.http.HttpClient;
+import com.aliyun.core.http.HttpMethod;
+import com.aliyun.core.http.ProxyOptions;
+import com.aliyun.httpcomponent.httpclient.ApacheAsyncHttpClientBuilder;
+import com.aliyun.sdk.service.dysmsapi20170525.models.*;
+import com.aliyun.sdk.service.dysmsapi20170525.*;
+import com.google.gson.Gson;
+import darabonba.core.RequestConfiguration;
+import darabonba.core.client.ClientOverrideConfiguration;
+import darabonba.core.utils.CommonUtil;
+import darabonba.core.TeaPair;
+
+//import javax.net.ssl.KeyManager;
+//import javax.net.ssl.X509TrustManager;
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.X509TrustManager;
+import javax.validation.constraints.Null;
+import java.net.InetSocketAddress;
+import java.time.Duration;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 @CrossOrigin
@@ -13,14 +36,90 @@ import java.util.Map;
 public class AccountController {
 
     @PostMapping("/account")
-    public String Register(@RequestBody Map data) {
+    public String Login(@RequestBody Map data) throws Exception {
         SecureRandom randomGen = new SecureRandom();
-        String code = String.format("%04d", randomGen.nextInt(1000));
+        String code = String.format("%04d", randomGen.nextInt(10000));
         String loginVal = data.get("loginVal").toString();
         String loginType = data.get("loginType").toString();
+        if(Objects.equals(loginType, "Phone")) {
+            // LoginPhone(loginVal, code);
+        } else if (Objects.equals(loginType, "Mail")) {
+            LoginMail(loginVal, code);
+        }
+        System.out.println(code);
         System.out.print(loginVal + ", " + loginType + '\n');
+        return code;
+    }
 
-        SendSms messageSender = new SendSms(loginVal, code);
-        return loginVal + ", " + loginType;
+    public Boolean LoginPhone(String phoneNo, String code) throws ExecutionException, InterruptedException {
+        // HttpClient Configuration
+        HttpClient httpClient = new ApacheAsyncHttpClientBuilder()
+                .connectionTimeout(Duration.ofSeconds(10)) // Set the connection timeout time, the default is 10 seconds
+                .responseTimeout(Duration.ofSeconds(10)) // Set the response timeout time, the default is 20 seconds
+                .maxConnections(128) // Set the connection pool size
+                .maxIdleTimeOut(Duration.ofSeconds(50)) // Set the connection pool timeout, the default is 30 seconds
+                // Configure the proxy
+                .proxy(new ProxyOptions(ProxyOptions.Type.HTTP, new InetSocketAddress("<your-proxy-hostname>", 9001))
+                        .setCredentials("<your-proxy-username>", "<your-proxy-password>"))
+                // If it is an https connection, you need to configure the certificate, or ignore the certificate(.ignoreSSL(true))
+                .x509TrustManagers(new X509TrustManager[]{})
+                .keyManagers(new KeyManager[]{})
+                .ignoreSSL(false)
+                .build();
+
+        // Configure Credentials authentication information, including ak, secret, token
+        StaticCredentialProvider provider = StaticCredentialProvider.create(Credential.builder()
+                .accessKeyId("LTAI5tFEgYToujjR4b4EAAt2")
+                .accessKeySecret("MIPXZ4lacj3Y020q1Hw2xuYB9BAiRo")
+                //.securityToken("<your-token>") // use STS token
+                .build());
+
+        // Configure the Client
+        AsyncClient client = AsyncClient.builder()
+                .region("cn-hangzhou") // Region ID
+                //.httpClient(httpClient) // Use the configured HttpClient, otherwise use the default HttpClient (Apache HttpClient)
+                .credentialsProvider(provider)
+                //.serviceConfiguration(Configuration.create()) // Service-level configuration
+                // Client-level configuration rewrite, can set Endpoint, Http request parameters, etc.
+                .overrideConfiguration(
+                        ClientOverrideConfiguration.create()
+                                .setEndpointOverride("dysmsapi.aliyuncs.com")
+                        //.setConnectTimeout(Duration.ofSeconds(30))
+                )
+                .build();
+
+        // Parameter settings for API request
+        SendSmsRequest sendSmsRequest = SendSmsRequest.builder()
+                .phoneNumbers(phoneNo)
+                .signName("seubooking")
+                .templateCode("SMS_460816095")
+                .templateParam("{\"code\":\""+code+"\"}")
+                // Request-level configuration rewrite, can set Http request parameters, etc.
+                // .requestConfiguration(RequestConfiguration.create().setHttpHeaders(new HttpHeaders()))
+                .build();
+
+        // Asynchronously get the return value of the API request
+        CompletableFuture<SendSmsResponse> response = client.sendSms(sendSmsRequest);
+        // Synchronously get the return value of the API request
+        SendSmsResponse resp = response.get();
+        if(resp == null) {
+            System.out.println("Test If Empty");
+        }
+        System.out.println(new Gson().toJson(resp));
+        // Asynchronous processing of return values
+        /*response.thenAccept(resp -> {
+            System.out.println(new Gson().toJson(resp));
+        }).exceptionally(throwable -> { // Handling exceptions
+            System.out.println(throwable.getMessage());
+            return null;
+        });*/
+        //System.out.println("Test If Reach");
+        // Finally, close the client
+        client.close();
+        return true;
+    }
+
+    public Boolean LoginMail(String addr, String code) {
+        return true;
     }
 }
